d04a57e42908ca5cc6f335935b3a349f04b2ceca
Neethu Issac <neethuissac23@gmail.com>
2023-03-29 23:54:57 +0530

    added

---
+++ ecommerceproject/cartapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'cartapp'
+
+urlpatterns = [
+    path('', views.cart_detail, name="cart_detail"),
+    path('add/<int:product_id>/', views.add_cart, name="add_cart"),
+    path('remove/<int:product_id>/', views.remove_cart, name="remove_cart"),
+    path('delete/<int:product_id>/', views.delete_cart, name="delete_cart"),
+]
